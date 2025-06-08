3f78a422ff0b5a9385164ba159c463c7706333e9
Shubham Thaware <shubham-thware@email.com>
2025-06-08 17:23:16 +0530

    Hashed output files

--- terraform-templates/aws-ec2/output.tf
+++ terraform-templates/aws-ec2/output.tf
@@ -1,12 +1,12 @@
-output "bastion_server_public_ip" {
-    description = "Public IP of the Bastion server"
-    value = aws_instance.bastion.public_ip
+# output "bastion_server_public_ip" {
+#     description = "Public IP of the Bastion server"
+#     value = aws_instance.bastion.public_ip
   
-}
+# }
 
 
-output "database_server_private_ip" {
-    description = "Private IP of the Database server"
-    value = aws_instance.database.private_ip
+# output "database_server_private_ip" {
+#     description = "Private IP of the Database server"
+#     value = aws_instance.database.private_ip
   
-}
+# }
--- terraform-templates/aws-vpc/output.tf
+++ terraform-templates/aws-vpc/output.tf
@@ -1,17 +1,17 @@
-output "vpc_id" {
-  value = aws_vpc.vpc.id
+# output "vpc_id" {
+#   value = aws_vpc.vpc.id
 
-}
+# }
 
-output "vpc_arn" {
-  value = aws_vpc.vpc.arn
+# output "vpc_arn" {
+#   value = aws_vpc.vpc.arn
 
-}
+# }
 
-output "private_subnet_ids" {
-  value = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
-}
+# output "private_subnet_ids" {
+#   value = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
+# }
 
-output "public_subnet_ids" {
-  value = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
-}
+# output "public_subnet_ids" {
+#   value = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+# }
