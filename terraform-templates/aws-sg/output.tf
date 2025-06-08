0607c1e253b154f2b9aaa5159d9dc0e3e20a6604
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-08 18:32:07 +0530

    Update output.tf

--- terraform-templates/aws-sg/output.tf
+++ terraform-templates/aws-sg/output.tf
@@ -1,14 +1,14 @@
 output "jump_sg_id" {
   description = "ID of the jump server security group"
-  value       = module.sg.bastion-sg.id
+  value       = module.sg.jump_sg_id
 }
 
 output "db_sg_id" {
   description = "ID of the database security group"
-  value       = module.sg.database-sg.id
+  value       = module.sg.db_sg_id
 }
 
 output "eks_cluster_sg_id" {
   description = "ID of the EKS cluster security group"
-  value       = module.sg.eks_cluster.id
+  value       = module.sg.eks_cluster_sg_id
 }
