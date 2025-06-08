faa4b319b5b063168f8ff3a59f93506291d41fe4
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-08 17:52:19 +0530

    Update output.tf

--- terraform-templates/aws-vpc/output.tf
+++ terraform-templates/aws-vpc/output.tf
@@ -3,14 +3,14 @@ output "vpc_id" {
 }
 
 output "vpc_arn" {
-  value = module.aws_vpc.vpc.arn
+  value = module.vpc.arn
 
 }
 
 output "private_subnet_ids" {
-  value = [module.aws_subnet.private-subnet-1.id, module.aws_subnet.private-subnet-2.id]
+  value = [module.vpc.private-subnet-1.id, module.aws_subnet.private-subnet-2.id]
 }
 
 output "public_subnet_ids" {
-  value = [module.aws_subnet.public-subnet-1.id, module.aws_subnet.public-subnet-2.id]
+  value = [module.vpc.public-subnet-1.id, module.aws_subnet.public-subnet-2.id]
 }
