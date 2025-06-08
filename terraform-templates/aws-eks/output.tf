a25b54337a5a4b7d59b3cd717cb44cdae2f49b96
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-08 18:31:24 +0530

    Update output.tf

--- terraform-templates/aws-eks/output.tf
+++ terraform-templates/aws-eks/output.tf
@@ -1,72 +1,68 @@
 # EKS Cluster Outputs
 output "cluster_id" {
   description = "The name/id of the EKS cluster."
-  value       = module.eks.cluster.id
+  value       = module.eks.cluster_id
 }
 
 output "cluster_arn" {
   description = "The Amazon Resource Name (ARN) of the cluster."
-  value       = module.eks.cluster.arn
+  value       = module.eks.cluster_arn
 }
 
 output "cluster_certificate_authority_data" {
   description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
-  value       = module.eks.cluster.certificate_authority[0].data
+  value       = module.eks.cluster_certificate_authority_data
 }
 
 output "cluster_endpoint" {
   description = "The endpoint for your EKS Kubernetes API."
-  value       = module.eks.cluster.endpoint
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_version" {
   description = "The Kubernetes server version for the EKS cluster."
-  value       = module.eks.cluster.version
+  value       = module.eks.cluster_version
 }
 
 output "cluster_iam_role_name" {
   description = "IAM role name of the EKS cluster."
-  value       = module.eks.cluster.name 
+  value       = module.eks.cluster_iam_role_name
 }
 
 output "cluster_iam_role_arn" {
   description = "IAM role ARN of the EKS cluster."
-  value       = module.eks.cluster.arn
+  value       = module.eks.cluster_iam_role_arn
 }
 
 output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster OIDC Issuer"
-  value       = module.eks.cluster.identity[0].oidc[0].issuer
+  value       = module.eks.cluster_oidc_issuer_url
 }
 
 output "cluster_primary_security_group_id" {
   description = "The cluster primary security group ID created by the EKS cluster on 1.14 or later. Referred to as 'Cluster security group' in the EKS console."
-  value       = module.eks.cluster.vpc_config[0].cluster_security_group_id
+  value       = module.eks.cluster_primary_security_group_id
 }
 
 # EKS Node Group Outputs - Public
 output "node_group_public_id" {
   description = "Public Node Group ID"
-  value       = module.eks.nodes.id
+  value       = module.eks.node_group_public_id
 }
 
 output "node_group_public_arn" {
   description = "Public Node Group ARN"
-  value       = module.eks.nodes.arn
+  value       = module.eks.node_group_public_arn
 }
 
 output "node_group_public_status" {
   description = "Public Node Group status"
-  value       = module.eks.nodes.status 
+  value       = module.eks.node_group_public_status
 }
 
 output "node_group_public_version" {
   description = "Public Node Group Kubernetes Version"
-  value       = module.eks.nodes.version
+  value       = module.eks.node_group_public_version
 }
 
-output "private_subnet_ids" {
-  description = "List of private subnet IDs used in EKS"
-  value       = module.aws_vpc_private_subnet_ids
-}
 
