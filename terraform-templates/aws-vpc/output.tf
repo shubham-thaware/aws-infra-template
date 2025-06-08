0260bbb0688bf9d17bbe9d1362a46c26ce347501
Shubham Thaware <shubham-thware@email.com>
2025-06-08 17:41:18 +0530

    update

--- terraform-templates/aws-vpc/output.tf
+++ terraform-templates/aws-vpc/output.tf
@@ -1,17 +1,16 @@
-# output "vpc_id" {
-#   value = aws_vpc.vpc.id
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
 
-# }
+output "vpc_arn" {
+  value = module.aws_vpc.vpc.arn
 
-# output "vpc_arn" {
-#   value = aws_vpc.vpc.arn
+}
 
-# }
+output "private_subnet_ids" {
+  value = [module.aws_subnet.private-subnet-1.id, module.aws_subnet.private-subnet-2.id]
+}
 
-# output "private_subnet_ids" {
-#   value = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
-# }
-
-# output "public_subnet_ids" {
-#   value = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
-# }
+output "public_subnet_ids" {
+  value = [module.aws_subnet.public-subnet-1.id, module.aws_subnet.public-subnet-2.id]
+}
