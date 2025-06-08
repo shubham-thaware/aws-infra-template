14159d384794908c2b178e85cfa76fd57fea44f4
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-08 17:54:35 +0530

    Update output.tf

--- terraform-templates/aws-ecr/output.tf
+++ terraform-templates/aws-ecr/output.tf
@@ -1,14 +1,14 @@
 output "ecr_repository_url" {
   description = "The URL of the ECR repository"
-  value       = module.aws_ecr_repository.this.repository_url
+  value       = module.ecr.this.repository_url
 }
 
 output "ecr_repository_arn" {
   description = "The ARN of the ECR repository"
-  value       = module.aws_ecr_repository.this.arn
+  value       = module.ecr.this.arn
 }
 
 output "ecr_repository_id" {
   description = "The Registry ID of the ECR repository"
-  value       = module.aws_ecr_repository.this.registry_id
+  value       = module.ecr.this.registry_id
 }
