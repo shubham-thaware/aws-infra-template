1c07066acbb368d762566f5122784efb7bd75f12
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-13 21:52:51 +0530

    Update main.tf

--- terraform-templates/aws-vpc/main.tf
+++ terraform-templates/aws-vpc/main.tf
@@ -3,9 +3,9 @@ module "vpc" {
   env            = var.env
   vpc_name       = var.vpc_name
   vpc_cidr_block = var.vpc_cidr_block
-  public-subnet-cidr-1 = var.public_subnet_cidr_1
-  public-subnet-cidr-2 = var.public_subnet_cidr_2
-  private-subnet-cidr-1 = var.private_subnet_cidr_1
-  private-subnet-cidr-2 = var.private_subnet_cidr_2
+ public_subnet_cidr_1 = var.public_subnet_cidr_1
+  public_subnet_cidr_2 = var.public_subnet_cidr_2
+  private_subnet_cidr_1 = var.private_subnet_cidr_1
+  private_subnet_cidr_2 = var.private_subnet_cidr_2
   #additional_tags     = var.additional_tags
 }
